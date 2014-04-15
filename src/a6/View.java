e2f0b96ac8b81358b52a6a60782ca3f1e7cd1b8e
iaj8 <iaj8@cornell.edu>
2014-04-14 21:50:11 -0400

    ish

--- src/a6/View.java
+++ src/a6/View.java
@@ -2,13 +2,19 @@ package a6;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
 
-public class View {
+public class View extends Canvas{
+	Paint background;
 	public View (Stage s){
+		background = Color.BLACK;
 		Group g = new Group();
 		Pane pane = new HBox();
 		VBox vbox = new VBox();
@@ -18,5 +24,9 @@ public class View {
 		s.setScene(sc);
 		s.setWidth(1500);
 		s.setHeight(900);
+		s.show();
+		GraphicsContext gc = getGraphicsContext2D();
+		gc.setFill(background);
+		gc.fillRect(0, 0, getWidth(), getHeight());
 	}
 }
