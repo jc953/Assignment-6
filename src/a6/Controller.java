cba7ab9cef9aca6cb5a2963907fd98a199e195d0
iaj8 <iaj8@cornell.edu>
2014-04-15 00:32:13 -0400

    main

--- src/a6/Controller.java
+++ src/a6/Controller.java
@@ -1,10 +1,12 @@
 package a6;
 import a5.*;
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class Controller {
 	Label stepLabel;
@@ -12,6 +14,7 @@ public class Controller {
 	View v;
 	CritterWorld cw;
 	Label critterLabel;
+	Button b1;
 	public Controller(View v, CritterWorld cw1){
 		this.v = v;
 		numSteps = 0;
@@ -20,21 +23,52 @@ public class Controller {
 	}
 	
 	void setWorldSteps(){
-		Button b = new Button("step");
+		Button b = new Button("Step Once");
+		b1 = new Button("Step Continuously");
 		stepLabel = new Label("Steps Advanced: 0");
 		critterLabel = new Label("Critters Alive: 0");
 		v.getVBox().getChildren().add(b);
+		v.getVBox().getChildren().add(b1);
 		v.getVBox().getChildren().add(stepLabel);
 		v.getVBox().getChildren().add(critterLabel);
 		b.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent _) {
-                cw.step();
-                numSteps++;
-                stepLabel.setText("Steps Advanced: " + numSteps);
-                critterLabel.setText("Critters Alive: " + cw.critters.size());
+                step();
             }
         });
+		b1.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent _){
+				Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000), 
+						new EventHandler<ActionEvent>(){
+						
+						@Override
+						public void handle(ActionEvent arg0){
+							if (b1.getText() == "Stop Stepping"){
+								step();
+							}
+						}
+				}));
+				timeline.setCycleCount(Timeline.INDEFINITE);
+				if(b1.getText() == "Step Continuously"){
+					b1.setText("Stop Stepping");
+					timeline.play();
+				}
+				else{
+					b1.setText("Step Continuously");
+					timeline.stop();
+				}
+
+			}
+		});
+	}
+	
+	void step(){
+    	cw.step();
+        numSteps++;
+        stepLabel.setText("Steps Advanced: " + numSteps);
+        critterLabel.setText("Critters Alive: " + cw.critters.size());
 	}
 
 }
